f6b9e877ed84a1959f3ac554010394a0e6af7cb5
darthvalinor <darthvalinor@gmail.com>
2018-04-22 23:46:09 +0300

    shorter var names

--- resistance.js
+++ resistance.js
@@ -1,4 +1,4 @@
-var ResistanceRules = {
+var Rules = {
     readyForStart: function(game) {
         if (game.numberOfPlayers < 5 || game.numberOfPlayers > 10) {
             alert('Number of players must be from 5 up to 10!')
@@ -9,7 +9,7 @@ var ResistanceRules = {
 
 };
 
-var ResistanceGame = function(numberOfPlayers,players) {
+var Game = function(numberOfPlayers,players) {
     this.numberOfPlayers = numberOfPlayers || 10;
     this.players = players || ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'].slice(0, this.numberOfPlayers);
     this.roles = ['fighter', 'merlin', 'percival', 'mordred', 'morgana', 'killer'];
